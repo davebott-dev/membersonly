445a63be7cb535ced365a4566caacae403cdc7dd
= <bottenbergdave@gmail.com>
2024-09-26 21:00:56 -0400

    added simple form validation to this page

--- client/src/routes/Sign-up.jsx
+++ client/src/routes/Sign-up.jsx
@@ -1,6 +1,22 @@
+import {useState, useRef} from 'react'
 import '../App.css'
 
 function SignUp() {
+  const [type,setType] =useState('submit');
+  const passwordRef = useRef();
+  const checkPasswordRef = useRef();
+
+  const checkPassword = () => {
+    if(passwordRef.current.value == checkPasswordRef.current.value ) {
+      setType("submit" );
+    } else {
+      setType("button");
+      alert('the passwords do not match!');
+      passwordRef.current.value ="";
+      checkPasswordRef.current.value ="";
+    }   
+  }
+
 
   return (
     <div>
@@ -9,14 +25,19 @@ function SignUp() {
       <form method="POST" action="/api">
           <div>
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
+            <input type="text" id="username" name="username" required />
           </div>
 
           <div>
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
+            <input type="password" id="password" name="password" ref={passwordRef} required />
           </div>
-          <button>Submit</button>
+
+          <div>
+            <label htmlFor="password">Re-enter Password:</label>
+            <input type="password" id="match" name="match" ref={checkPasswordRef} required />
+          </div>
+          <button type={type} onClick ={checkPassword}>Submit</button>
       </form>
     </div>
   )
